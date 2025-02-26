b42d09c05c44ceba9270b79da968374482741029
Phuoc Dinh <phuocdinh462@gmail.com>
2025-02-26 23:45:57 +0700

    refactor: implement better solution for 20.valid-parentheses

--- 20.valid-parentheses.ts
+++ 20.valid-parentheses.ts
@@ -7,14 +7,33 @@
 // @lc code=start
 function isValid(s: string): boolean {
   if (s.length % 2 !== 0) return false;
+  else if (s[0] === ")" || s[0] === "]" || s[0] === "}") return false;
+  else if (s[s.length - 1] === "(" || s[s.length - 1] === "[" || s[s.length - 1] === "{")
+    return false;
 
-  let prevLength = 0;
+  const stack: string[] = [];
 
-  while (prevLength !== s.length) {
-    prevLength = s.length;
-    s = s.replace(/\(\)|\[\]|\{\}/g, "");
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
+      case "(":
+      case "[":
+      case "{":
+        stack.push(s[i]);
+        break;
+      case ")":
+        if (stack[stack.length - 1] === "(") stack.pop();
+        else return false;
+        break;
+      case "]":
+        if (stack[stack.length - 1] === "[") stack.pop();
+        else return false;
+        break;
+      default:
+        if (stack[stack.length - 1] === "{") stack.pop();
+        else return false;
+    }
   }
 
-  return s.length === 0;
+  return stack.length === 0;
 }
 // @lc code=end
