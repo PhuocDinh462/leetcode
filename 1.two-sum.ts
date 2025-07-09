8aad024c5080f1209411f6fb3f5a1059566a8590
Phuoc Dinh <phuocdinh462@gmail.com>
2025-07-09 15:12:49 +0700

    refactor: optimize twoSum function by replacing Set with Map for improved performance

--- 1.two-sum.ts
+++ 1.two-sum.ts
@@ -6,19 +6,13 @@
 
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
-  const setInt = new Set<number>();
-  let arr = Array<number>();
-  let i: number;
+  const map = new Map<number, number>();
 
-  for (i = 0; i < nums.length; i++) {
-    if (setInt.has(target - nums[i])) break;
-    setInt.add(nums[i]);
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) return [map.get(target - nums[i])!, i];
+    map.set(nums[i], i);
   }
 
-  for (let j = 0; j < i; j++) {
-    if (target - nums[i] === nums[j]) arr.push(j, i);
-  }
-
-  return arr;
+  return [];
 }
 // @lc code=end
