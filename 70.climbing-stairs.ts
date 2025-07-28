4656662d36253ea6a6325d340b75db5433c2cefc
Phuoc Dinh <phuocdinh462@gmail.com>
2025-07-28 10:57:18 +0700

    feat: 70

---
+++ 70.climbing-stairs.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=70 lang=typescript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+  if (n <= 2) return n;
+
+  const dp = new Array(n).fill(0);
+  dp[0] = 1;
+  dp[1] = 2;
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n - 1];
+}
+// @lc code=end
