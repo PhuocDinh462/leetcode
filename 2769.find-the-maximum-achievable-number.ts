9f7990ed34aef54b6be556955b1078d461723c5c
Phuoc Dinh <phuocdinh462@gmail.com>
2025-07-31 14:00:13 +0700

    feat: 2769

---
+++ 2769.find-the-maximum-achievable-number.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=2769 lang=typescript
+ *
+ * [2769] Find the Maximum Achievable Number
+ */
+
+// @lc code=start
+function theMaximumAchievableX(num: number, t: number): number {
+  return num + t * 2;
+}
+// @lc code=end
